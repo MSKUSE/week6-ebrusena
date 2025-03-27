834796e5611d6f95761fd97d33cbfc419b1e4c6b
ebrusena <ebrusenaunsal@posta.mu.edu.tr>
2025-03-27 13:32:33 +0300

    yotum

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,58 +1,79 @@
-public class Rectangle {
+public class Rectangle extends Shape(Point location, int sideA, int sideB){
+        super(location);
+set sideA
 
-    private Point topLeft;
-    private int sideA , sideB;
+private Point topLeft;
+        private int sideA , sideB;
+public static int counter = 0;
 
-    public Rectangle(Point topLeft, int sideA, int sideB) {
-        this.topLeft = topLeft;
-        setSideA(sideA);
-        setSideB(sideB);
-    }
+public static int counterforobject = 0;
 
-    public Rectangle(Point topLeft, int sideA) {
-        this.topLeft = topLeft;
-        setSideA(sideA);
-        setSideB(sideA);
-    }
+public static String class_name = "Rectangle";
 
-    public Point getTopLeft() {
-        return topLeft;
-    }
+public Rectangle(Point topLeft, int sideA, int sideB) {
+    this.topLeft = topLeft;
+    setSideA(sideA);
+    setSideB(sideB);
+    counter++;
+    counterforobject++;
+}
+public Rectangle(Point topLeft, int sideA) {
+    this.topLeft = topLeft;
+    setSideA(sideA);
+    setSideB(sideA);
+    counter++;
+    counterforobject++;
+}
+public Point getTopLeft() {
+    return topLeft;
+}
 
-    public void setTopLeft(Point topLeft) {
-        this.topLeft = topLeft;
-    }
+public void setTopLeft(Point topLeft) {
+    this.topLeft = topLeft;
+}
 
-    public int getSideA() {
-        return sideA;
-    }
+public int getSideA() {
+    return sideA;
+}
 
-    public void setSideA(int sideA) {
-        if (sideA < 0){
-            this.sideA = 0;
-            System.out.println("Side A can't be negative!!");
-        }
-        else {
-            this.sideA = sideA;
-        }
+public void setSideA(int sideA) throws IllegalAccessException {
+    if (sideA < 0){
+        throw new IllegalAccessException("Side A can't be negative!!");
     }
+    else {
+        this.sideA = sideA;
+    }
+}
 
-    public int getSideB() {
-        return sideB;
-    }
+public int getSideB() {
+    return sideB;
+}
 
-    public void setSideB(int sideB) {
-        if (sideB < 0) {
-            this.sideB = 0;
-            System.out.println("Side B can't be negative!!");
-        } else {
-            this.sideB = sideB;
-        }
+public void setSideB(int sideB) {
+    if (sideB < 0) {
+        this.sideB = 0;
+        System.out.println("Side B can't be negative!!");
+    } else {
+        this.sideB = sideB;
     }
-    public int perimeter(){
-        return 2 * (sideA + sideB);
-    }
-    public int area(){
-        return sideA * sideB;
-    }
+}
+public int perimeter(){
+    return 2 * (sideA + sideB);
+}
+
+@Override
+public String toString() {
+    return "Rectangle{" +
+            "topLeft=" + topLeft +
+            ", sideA=" + sideA +
+            ", sideB=" + sideB +
+            '}';
+}
+
+public double area(){
+    return sideA * sideB;
+
+
+}
+
 }
---
+++ src/Triangle.java
@@ -0,0 +1,17 @@
+public class Triangle extends Shape{
+    public static void main(String[] args) {
+        private Point a = new Point(2);
+        private Point b = new Point(1);
+        private Point c = new Point(3);
+
+        public Point getA() {
+            return a;
+        }
+
+        public void setA(Point a) {
+            this.a = a;
+            return;
+            System.out.println((a * b) / 2);
+
+        }
+    }
